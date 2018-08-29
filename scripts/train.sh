5c50eee2b1660dd4f0b9094c67616d970d2f36b4
anna shcherbina <annashcherbina@gmail.com>
2018-08-29 13:11:18 -0700

    finished exapmle

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=7 kerasAC_train --train_path /srv/scratch/annashch/deeplear
 		    --batch_size 1000 \
 		    --num_tasks 6 \
 		    --init_weights /srv/scratch/annashch/deeplearning/encode-roadmap.dnase_tf-chip.batch_256.params.npz \
-		    --architecture_spec basset_architecture_multitask_tfs.py
+		    --architecture_spec basset_architecture_multitask_tfs
 #example 2
 CUDA_VISIBLE_DEVICES=3 kerasAC_train --train_path /srv/scratch/annashch/deeplearning/form_inputs/gecco_inputs_v2/archive/single.train.28.bed \
 		    --valid_path /srv/scratch/annashch/deeplearning/form_inputs/gecco_inputs_v2/archive/single.validate.28.bed \
@@ -17,6 +17,6 @@ CUDA_VISIBLE_DEVICES=3 kerasAC_train --train_path /srv/scratch/annashch/deeplear
 		    --num_valid 10000 \
 		    --epochs 150 \
 		    --patience 150 \
-		    --architecture_spec basset_architecture_single_task.py
+		    --architecture_spec basset_architecture_single_task
 
 
