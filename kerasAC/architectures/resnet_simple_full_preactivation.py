045105c1d5d53a7cc8622def8d812085c884bd08
anna shcherbina <annashcherbina@gmail.com>
2018-08-30 20:19:39 -0700

    updated losses to use passed in w0 argument to determine whether weighted binary crossentropy or non-weighted binary crossentropy should be used

--- kerasAC/architectures/resnet_simple_full_preactivation.py
+++ kerasAC/architectures/resnet_simple_full_preactivation.py
@@ -64,7 +64,10 @@ def getModelGivenModelOptionsAndWeightInits(w0,w1,init_weights=None,checkpoint_w
 
     adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     print("compiling!")
-    import kerasAC.custom_losses
-    loss=kerasAC.custom_losses.get_weighted_binary_crossentropy(w0_weights=w0,w1_weights=w1)
+    if w0!=None:
+        import kerasAC.custom_losses
+        loss=kerasAC.custom_losses.get_weighted_binary_crossentropy(w0_weights=w0,w1_weights=w1)
+    else:
+        loss="binary_crossentropy"
     model.compile(optimizer=adam,loss=loss,metrics=[positive_accuracy,negative_accuracy,precision,recall])
     return model
