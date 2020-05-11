30ab75401d341989266aa80a03e74bb4aa781243
anna shcherbina <annashcherbina@gmail.com>
2020-05-11 16:56:49 -0700

    example for weight loss generation

--- examples/tiledb_predict_examples/predict.sh
+++ examples/tiledb_predict_examples/predict.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-fold=0
-CUDA_VISIBLE_DEVICES=2 kerasAC_predict_tdb \
+
+#fold to use for training 
+fold=$1
+
+#countweight
+weight=$2
+
+#gpu to use for training 
+gpu=$3
+
+#set seed for training
+if [ -z "$4" ]
+then
+    seed=1234
+else
+    seed=$4
+fi
+echo "seed:$seed"
+
+#output directory 
+if [ -z "$5" ]
+then
+    outdir='.'
+else
+    outdir=$5
+fi
+echo "outdir:$outdir"
+
+
+CUDA_VISIBLE_DEVICES=$gpu kerasAC_predict_tdb \
 		    --batch_size 20 \
-		    --ref_fasta GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta \
+		    --ref_fasta /mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta \
 		    --tdb_array /srv/scratch/annashch/encode_dnase_tiledb/db/dnase \
 		    --tdb_partition_attribute_for_upsample idr_peak \
 		    --tdb_partition_thresh_for_upsample 2 \
@@ -16,13 +44,13 @@ CUDA_VISIBLE_DEVICES=2 kerasAC_predict_tdb \
 		    --tdb_output_transformation None log \
 		    --num_inputs 1 \
 		    --num_outputs 2 \
-		    --chrom_sizes hg38.chrom.sizes \
+		    --chrom_sizes ~/hg38.chrom.sizes \
 		    --tiledb_stride 50 \
 		    --fold $fold \
 		    --genome hg38 \
 		    --upsample_ratio_list_predict 1 \
-		    --predictions_and_labels_hdf5 predictions.k562.withdups.highweightcounts.$fold \
-		    --load_model_hdf5 K562.profile.peaks.only.bpnet.withdups.highcounts.0.hdf5 \
+		    --predictions_and_labels_hdf5 $outdir/predictions.k562.withdups.$seed\seed.$weight\counts.$fold \
+		    --load_model_hdf5 $outdir/K562.profile.peaks.only.bpnet.withdups.$seed\seed.$weight\counts.$fold.hdf5 \
 		    --tasks ENCSR000EOT \
 		    --upsample_threads 1 \
 		    --tdb_ambig_attribute ambig_peak \
--- examples/tiledb_train_examples/train.sh
+++ examples/tiledb_train_examples/train.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
-fold=0
-CUDA_VISIBLE_DEVICES=0 kerasAC_train \
+
+#fold to use for training 
+fold=$1
+
+#countweight
+weight=$2
+
+#gpu to use for training 
+gpu=$3
+
+#set seed for training
+if [ -z "$4" ]
+then
+    seed=1234
+else
+    seed=$4
+fi
+echo "seed:$seed"
+
+#output directory 
+if [ -z "$5" ]
+then
+    outdir='.'
+else
+    outdir=$5
+fi
+echo "outdir:$outdir"
+CUDA_VISIBLE_DEVICES=$gpu kerasAC_train \
+		    --seed $seed \
 		    --batch_size 20 \
 		    --ref_fasta /mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta \
 		    --tdb_array /srv/scratch/annashch/encode_dnase_tiledb/db/dnase \
-		    --tdb_partition_attribute_for_upsample idr_peak \
+		    --tdb_partition_attribute_for_upsample overlap_peak \
 		    --tdb_partition_thresh_for_upsample 1 \
 		    --tdb_input_source_attribute seq \
 		    --tdb_input_aggregation None \
@@ -15,6 +42,10 @@ CUDA_VISIBLE_DEVICES=0 kerasAC_train \
 		    --tdb_output_aggregation None sum \
 		    --tdb_output_transformation None log \
 		    --tdb_ambig_attribute ambig_peak \
+		    --tdb_input_min None \
+		    --tdb_input_max None \
+		    --tdb_output_min None 4.6 \
+		    --tdb_output_max None 11.5 \
 		    --num_inputs 1 \
 		    --num_outputs 2 \
 		    --fold $fold \
@@ -27,9 +58,9 @@ CUDA_VISIBLE_DEVICES=0 kerasAC_train \
 		    --max_queue_size 20 \
 		    --patience 3 \
 		    --patience_lr 2 \
-		    --model_prefix K562.profile.peaks.only.bpnet.withdups.highcounts.$fold \
+		    --model_prefix $outdir/K562.profile.peaks.only.bpnet.withdups.$seed\seed.$weight\counts.$fold \
 		    --architecture_spec profile_bpnet_dnase \
-		    --model_params model_params.countshighweight.txt \
+		    --model_params $outdir/params.txt \
 		    --use_multiprocessing False \
 		    --tasks ENCSR000EOT \
 		    --upsample_ratio_list_train 1.0 \
