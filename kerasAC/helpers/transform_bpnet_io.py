8d7868f162de24f9e08df74c1905cf7423a7d4f9
anna shcherbina <annashcherbina@gmail.com>
2020-08-07 22:37:00 -0700

    changed expit to softmax and normalize set to use l1 normal

--- kerasAC/helpers/transform_bpnet_io.py
+++ kerasAC/helpers/transform_bpnet_io.py
@@ -1,31 +1,31 @@
 import pandas as pd
 import numpy as np 
-from scipy.special import expit,logit
+from scipy.special import softmax,logit,softmax
 from sklearn.preprocessing import normalize
 
 def get_count_track_from_bpnet(profile_preds,count_preds):
     #get counts profile & softmax 
-    profile_softmax=expit(profile_preds)
+    profile_softmax=softmax(profile_preds,axis=1)
     return profile_softmax*np.expand_dims(count_preds,axis=1)
 
 def get_probability_track_from_bpnet(profile_preds):
-    return expit(profile_preds)
+    return softmax(profile_preds,axis=1)
 
 def get_logit_label_track(profile_labels,pseudocount=1e-4):
     return logit(profile_labels+pseudocount)
 
 def get_probability_label_track(labels):
-    return normalize(labels)
+    return normalize(labels,norm='l1')
 
 def get_model_outputs_to_plot(preds,coords=None):
     #label sum
-    labels_sum=preds['lab_1'] 
+    labels_sum=np.array(preds[('lab_1')]) 
     #label counts
-    labels_profile_counts=preds['lab_0']
+    labels_profile_counts=np.array(preds[('lab_0')])
     #prediction sum
-    predictions_sum=preds['pred_1']
+    predictions_sum=np.array(preds[('pred_1')])
     #prediction logits
-    predictions_profile_logits=preds['pred_0']
+    predictions_profile_logits=np.array(preds[('pred_0')])
     if coords is not None:
         labels_sum=labels_sum[coords]
         labels_profile_counts=labels_profile_counts[coords]
