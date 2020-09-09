9fa6a985f490d1fbc45f480ff2f078254ceed749
anna shcherbina <annashcherbina@gmail.com>
2020-09-09 02:23:53 -0700

    tf2 compat for hdf5

--- kerasAC/architectures/functional_basset_regression.py
+++ kerasAC/architectures/functional_basset_regression.py
@@ -2,7 +2,11 @@ import numpy as np ;
 from kerasAC.metrics import *
 from kerasAC.custom_losses import * 
 
+
 def getModelGivenModelOptionsAndWeightInits(args):
+    import tensorflow as tf
+    tf.compat.v1.enable_eager_execution()
+
     #get the arguments
     seed=args.seed
     w0=args.w0
@@ -27,34 +31,34 @@ def getModelGivenModelOptionsAndWeightInits(args):
     #print(K.image_data_format())
 
     seq = Input(shape=(1,1000,4))
-    if (init_weights!=None):
-        print("HERE")
-        #load the weight initializations
-        data=np.load(init_weights);
-        x = Conv2D(filters=300,kernel_size=(1,19),weights=[data['0.Conv/weights:0'],np.zeros(300,)],padding="same")(seq)
-        x = BatchNormalization(axis=-1,weights=[data['2.BatchNorm/gamma:0'],data['1.BatchNorm/beta:0'],np.zeros(300,),np.zeros(300,)])(x)
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(1,3))(x)
-        x = Conv2D(filters=200,kernel_size=(1,11),weights=[data['3.Conv_1/weights:0'],np.zeros(200,)],padding="same")(x)
-        x = BatchNormalization(axis=-1,weights=[data['5.BatchNorm_1/gamma:0'],data['4.BatchNorm_1/beta:0'],np.zeros(200,),np.zeros(200,)])(x)
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(1,4))(x)
-        x = Conv2D(filters=200,kernel_size=(1,7),weights=[data['6.Conv_2/weights:0'],np.zeros(200,)],padding="same")(x)
-        x = BatchNormalization(axis=-1,weights=[data['8.BatchNorm_2/gamma:0'],data['7.BatchNorm_2/beta:0'],np.zeros(200,),np.zeros(200,)])(x)
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(1,4))(x)
+    if (init_weights is not None):
+        with tf.init_scope():
+            #load the weight initializations
+            data=np.load(init_weights);
+            x = Conv2D(filters=300,kernel_size=(1,19),weights=[data['0.Conv/weights:0'],np.zeros(300,)],padding="same")(seq)
+            x = BatchNormalization(axis=-1,weights=[data['2.BatchNorm/gamma:0'],data['1.BatchNorm/beta:0'],np.zeros(300,),np.zeros(300,)])(x)
+            x = Activation('relu')(x)
+            x = MaxPooling2D(pool_size=(1,3))(x)
+            x = Conv2D(filters=200,kernel_size=(1,11),weights=[data['3.Conv_1/weights:0'],np.zeros(200,)],padding="same")(x)
+            x = BatchNormalization(axis=-1,weights=[data['5.BatchNorm_1/gamma:0'],data['4.BatchNorm_1/beta:0'],np.zeros(200,),np.zeros(200,)])(x)
+            x = Activation('relu')(x)
+            x = MaxPooling2D(pool_size=(1,4))(x)
+            x = Conv2D(filters=200,kernel_size=(1,7),weights=[data['6.Conv_2/weights:0'],np.zeros(200,)],padding="same")(x)
+            x = BatchNormalization(axis=-1,weights=[data['8.BatchNorm_2/gamma:0'],data['7.BatchNorm_2/beta:0'],np.zeros(200,),np.zeros(200,)])(x)
+            x = Activation('relu')(x)
+            x = MaxPooling2D(pool_size=(1,4))(x)
 
-        x = Flatten()(x)
-        x = Dense(1000,weights=[data['9.fc0/fully_connected/weights:0'],np.zeros(1000,)])(x)
-        x = BatchNormalization(axis=1,weights=[data['11.fc0/BatchNorm/gamma:0'],data['10.fc0/BatchNorm/beta:0'],np.zeros(1000,),np.zeros(1000,)])(x)
-        x = Activation('relu')(x)
-        x = Dropout(0.3)(x)
+            x = Flatten()(x)
+            x = Dense(1000,weights=[data['9.fc0/fully_connected/weights:0'],np.zeros(1000,)])(x)
+            x = BatchNormalization(axis=1,weights=[data['11.fc0/BatchNorm/gamma:0'],data['10.fc0/BatchNorm/beta:0'],np.zeros(1000,),np.zeros(1000,)])(x)
+            x = Activation('relu')(x)
+            x = Dropout(0.3)(x)
 
-        x = Dense(1000,weights=[data['12.fc1/fully_connected/weights:0'],np.zeros(1000,)])(x)
-        x = BatchNormalization(axis=1,weights=[data['14.fc1/BatchNorm/gamma:0'],data['13.fc1/BatchNorm/beta:0'],np.zeros(1000,),np.zeros(1000,)])(x)
-        x = Activation('relu')(x)
-        x = Dropout(0.3)(x)
-        outputs = Dense(ntasks)(x)
+            x = Dense(1000,weights=[data['12.fc1/fully_connected/weights:0'],np.zeros(1000,)])(x)
+            x = BatchNormalization(axis=1,weights=[data['14.fc1/BatchNorm/gamma:0'],data['13.fc1/BatchNorm/beta:0'],np.zeros(1000,),np.zeros(1000,)])(x)
+            x = Activation('relu')(x)
+            x = Dropout(0.3)(x)
+            outputs = Dense(ntasks)(x)
 
 
     else:
@@ -91,6 +95,7 @@ def getModelGivenModelOptionsAndWeightInits(args):
     loss=ambig_mean_squared_error 
     model = Model(inputs = [seq], outputs = outputs)
     model.compile(optimizer=adam,loss=loss)
+    data=np.load(init_weights);                
     return model
 
 if __name__=="__main__":
