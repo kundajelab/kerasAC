03ee26cc51c2af36126aba2d4b1da3260a7100a3
Anna Shcherbina <annashcherbina@gmail.com>
2021-11-17 17:01:13 +0000

    updates

--- kerasAC/interpret/run_modisco.py
+++ kerasAC/interpret/run_modisco.py
@@ -22,20 +22,21 @@ def main():
     null_per_pos_scores = modisco.coordproducers.LaplaceNullDist(num_to_samp=20000)
 
     tfmodisco_results = modisco.tfmodisco_workflow.workflow.TfModiscoWorkflow(
-                        max_seqlets_per_metacluster=args.seqlets,
-                        sliding_window_size=15,
-                        flank_size=5,
-                        target_seqlet_fdr=0.05,
-                        seqlets_to_patterns_factory=modisco.tfmodisco_workflow.seqlets_to_patterns.TfModiscoSeqletsToPatternsFactory(
-                            n_cores=args.cores,
-                            trim_to_window_size=15,
-                            initial_flank_to_add=5,
-                            final_min_cluster_size=50))(task_names=["task0"],
-                                contrib_scores={'task0': impscores},
-                                hypothetical_contribs={'task0': hyp_impscores},
-                                null_per_pos_scores=null_per_pos_scores,
-                                one_hot=onehot_seqs)
-
+        max_seqlets_per_metacluster=args.seqlets,
+        sliding_window_size=21,
+        flank_size=10,
+        target_seqlet_fdr=0.05,
+        min_passing_windows_frac=0.03,
+        seqlets_to_patterns_factory=modisco.tfmodisco_workflow.seqlets_to_patterns.TfModiscoSeqletsToPatternsFactory(
+            n_cores=args.cores,
+            trim_to_window_size=30,
+            initial_flank_to_add=10,
+            final_min_cluster_size=30))(task_names=["task0"],
+                                        contrib_scores={'task0': impscores},
+                                        hypothetical_contribs={'task0': hyp_impscores},
+                                        null_per_pos_scores=null_per_pos_scores,
+                                        one_hot=onehot_seqs)
+    
     h5f = h5py.File(args.outfile, 'w')
     tfmodisco_results.save_hdf5(h5f)
     h5f.close()
